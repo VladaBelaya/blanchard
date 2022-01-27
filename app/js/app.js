13cfdc3a9002b44257df2c77707ce1c291c68242
VladaBelaya <vlada_chvanova@mail.ru>
2022-01-27 20:04:42 +0300

    completed the layout of the ".gallery__select" block and navigation in ".gallery__slider"

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,8 @@
 import initDropdown from './dropdown.js'
 import initSwiper from './swiper.js'
+import initSelect from './select.js'
 document.addEventListener('DOMContentLoaded', () => {
 	initDropdown()
 	initSwiper()
+	initSelect()
 })
