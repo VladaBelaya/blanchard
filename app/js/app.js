98a6db90c6c27a38e17ea0df4fe6feb61ca831fc
VladaBelaya <vlada_chvanova@mail.ru>
2022-01-29 14:18:11 +0300

    completed tab flag

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,14 @@
 import initDropdown from './features/dropdown/dropdown.js'
 import initHeaderSlider from './features/header-slideshow/header-slideshow.js'
 import initSelect from './features/select/select.js'
-import initFilterGallery from "./features/filter-gallery/filter-gallery.js";
-import initSliderGallery from "./features/slider-gallery/slider-gallery.js";
-
+import initFilterGallery from './features/filter-gallery/filter-gallery.js';
+import initSliderGallery from './features/slider-gallery/slider-gallery.js';
+import initTabs from './features/catalog-tabs/catalogTabs.js'
 document.addEventListener('DOMContentLoaded', () => {
 	initDropdown()
 	initHeaderSlider()
 	initSelect()
 	initFilterGallery()
 	initSliderGallery()
+	initTabs()
 })
